448f2aa003f91bd0475f46c82f99e0d0682db0b3
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-10 11:52:28 +0100

    Add reporter toggle component

--- Pin.LiveSports.Blazor/Hubs/ReportHub.cs
+++ Pin.LiveSports.Blazor/Hubs/ReportHub.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Pin.LiveSports.Core.Entities.Reports;
 
 namespace Pin.LiveSports.Blazor.Hubs
 {
     public class ReportHub : Hub
     {
-        public async Task Send(string name, string message)
+        public async Task Send(Report report)
         {
-            await Clients.All.SendAsync("report", name, message);
+            await this.Clients.Others.SendAsync("reportAdded", report);
         }
     }
 }
