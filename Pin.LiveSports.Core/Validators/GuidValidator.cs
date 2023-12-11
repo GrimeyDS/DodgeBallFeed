50dfb77c6231b5879c4133d272b1bea00b3a0ed6
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-11 21:29:48 +0100

    Add guid validation

---
+++ Pin.LiveSports.Core/Validators/GuidValidator.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Pin.LiveSports.Core.Validators
+{
+    public class GuidValidator : ValidationAttribute
+    {
+
+        public Guid Guid { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var guid = (Guid)value;
+
+            if (guid == Guid.Empty)
+            {
+                return new ValidationResult("Please select an Team/Player");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
