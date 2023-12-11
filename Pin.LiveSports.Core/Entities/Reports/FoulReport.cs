d72fd63c5fa218eaa16cf1c5664f417205db112c
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-11 21:33:46 +0100

    Add data annotations

--- Pin.LiveSports.Core/Entities/Reports/FoulReport.cs
+++ Pin.LiveSports.Core/Entities/Reports/FoulReport.cs
@@ -1,8 +1,11 @@
 ﻿
+using Pin.LiveSports.Core.Validators;
+
 namespace Pin.LiveSports.Core.Entities.Reports
 {
     public class FoulReport : GeneralReport
     {
+        [GuidValidator]
         public Guid PlayerId { get; set; }
 
     }
--- Pin.LiveSports.Core/Entities/Reports/GeneralReport.cs
+++ Pin.LiveSports.Core/Entities/Reports/GeneralReport.cs
@@ -1,12 +1,17 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Pin.LiveSports.Core.Entities.Reports
 {
     public class GeneralReport
     {
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public TimeOnly Time { get; set; }
+        [Required]
         public string ReportMessage { get; set; }
     }
 }
