3ad5c0050d4486ef2554fa8a80a88842e41179ff
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-12 16:57:14 +0100

    Add select components to teamchange

--- Pin.LiveSports.Core/Entities/Reports/TeamChangeReport.cs
+++ Pin.LiveSports.Core/Entities/Reports/TeamChangeReport.cs
@@ -1,12 +1,14 @@
 ﻿
-using Pin.LiveSports.Core.Entities.Games;
+using Pin.LiveSports.Core.Validators;
 
 
 namespace Pin.LiveSports.Core.Entities.Reports
 {
     public class TeamChangeReport : GeneralReport
     {
+        [GuidValidator]
         public Guid PlayerId { get; set; }
+        [GuidValidator]
         public Guid TeamId { get; set; }
         public bool IsOut { get; set; } = false;
     }
