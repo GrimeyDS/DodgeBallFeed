ced7cd5caf9510f4c78e1b64497887418303e758
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-12 18:53:43 +0100

    Add score report

---
+++ Pin.LiveSports.Core/Entities/Reports/ScoreReport.cs
@@ -0,0 +1,10 @@
+﻿namespace Pin.LiveSports.Core.Entities.Reports
+{
+    public class ScoreReport : GeneralReport
+    {
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+
+        public Guid ScoredTeam { get; set; }
+    }
+}
