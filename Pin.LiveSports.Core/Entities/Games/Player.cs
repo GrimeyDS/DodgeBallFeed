644198f267830118fdebb99f63a1e013f210656e
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-14 20:42:37 +0100

    Add modal component for player

--- Pin.LiveSports.Core/Entities/Games/Player.cs
+++ Pin.LiveSports.Core/Entities/Games/Player.cs
@@ -8,6 +8,7 @@ namespace Pin.LiveSports.Core.Entities.Games
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Country { get; set; }
+        public string Description { get; set; } = "Awesome player that does awesome stuff";
         public string PictureUrl { get; set; }
         public bool IsOut { get; set; }
         public Role Role { get; set; }
