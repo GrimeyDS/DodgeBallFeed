3154c11336852b401423dafb65f129f4df13272a
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-15 09:17:02 +0100

    Add code seperation and base component to forms

---
+++ Pin.LiveSports.Blazor/Components/Reporting/FormComponents/BaseFormComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pin.LiveSports.Core.Entities.Games;
+using Pin.LiveSports.Core.Entities.Reports;
+
+namespace Pin.LiveSports.Blazor.Components.Reporting.FormComponents
+{
+    public abstract class BaseFormComponent : ComponentBase
+    {
+        [Parameter]
+        public EventCallback<GeneralReport> OnSave { get; set; }
+
+        [Parameter]
+        public Team[] Teams { get; set; }
+
+        [Parameter]
+        public Game Game { get; set; }
+    }
+}
---
+++ Pin.LiveSports.Blazor/Components/Reporting/FormComponents/TeamChangeForm.Razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pin.LiveSports.Core.Entities.Games;
+using Pin.LiveSports.Core.Entities.Reports;
+
+namespace Pin.LiveSports.Blazor.Components.Reporting.FormComponents
+{
+    public partial class TeamChangeForm
+    {
+        [Parameter]
+        public TeamChangeReport ReportItem { get; set; }
+
+        private Player[] teamPlayers;
+
+        private void SetTeam(Guid id)
+        {
+            ReportItem.Team = Teams.FirstOrDefault(t => t.Id == id);
+            if (ReportItem.Team is not null)
+                teamPlayers = ReportItem.Team.Players.ToArray();
+        }
+
+        private void SetPlayer(Guid id)
+        {
+            ReportItem.Player = teamPlayers.FirstOrDefault(p => p.Id == id);
+        }
+
+        private void SetGamePlayer()
+        {
+            ReportItem.IsOut = ReportItem.Player.IsOut;
+            OnSave.InvokeAsync(ReportItem);
+        }
+    }
+}
