f386faf280d4b6842baeede2518d1380a6cfaa72
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-15 14:34:50 +0100

    Add comment for primary constructors

--- Pin.LiveSports.Core/Entities/Reports/GeneralReport.cs
+++ Pin.LiveSports.Core/Entities/Reports/GeneralReport.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Pin.LiveSports.Core.Entities.Reports
 {
+    // Gebruik van Prime Constructor:
+    // Zet de meegegeven parameters in scope van heel de class. Hierdoor kunnen ze direct gebruikt worden voor initializering.
+    // Dit verminderd lijnen code en lijkt me super interessant bij dependency injection.
     public class GeneralReport(TimeOnly currentGameTime) : IValidatableObject
     {
         [Required]
--- Pin.LiveSports.Core/Entities/Reports/WinReport.cs
+++ Pin.LiveSports.Core/Entities/Reports/WinReport.cs
@@ -3,6 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Pin.LiveSports.Core.Entities.Reports
 {
+    // Gebruik van Prime Constructor:
+    // Zet de meegegeven parameters in scope van heel de class. Hierdoor kunnen ze direct gebruikt worden voor initializering.
+    // Dit verminderd lijnen code en lijkt me super interessant bij dependency injection.
+    // In dit voorbeeld roept hij ook de base primary constructor op in general report.
     public class WinReport(TimeOnly currentGameTime) : GeneralReport(currentGameTime)
     {
         public Team WinningTeam { get; set; }
