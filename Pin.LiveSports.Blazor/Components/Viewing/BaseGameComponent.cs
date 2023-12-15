5c4d4a62d50151efe04ed4542bd2f9ed0e28b7af
SvenDS91 <sven.de.schagt@student.howest.be>
2023-12-15 13:42:25 +0100

    Add code seperation, base component and code cleanup

---
+++ Pin.LiveSports.Blazor/Components/Viewing/BaseGameComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pin.LiveSports.Core.Entities.Games;
+
+namespace Pin.LiveSports.Blazor.Components.Viewing
+{
+    public class BaseGameComponent : ComponentBase
+    {
+        [Parameter]
+        public Game Game { get; set; }
+    }
+}
